2bbcc1d7bc10e316d0da3aec759f32d39a47456f
gillmourtunhira <gillmourtunhira@gmail.com>
2022-01-21 02:59:26 +0200

    hero layout

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,8 @@
 	import Skills from "./routes/Skills.svelte";
 	import Work from "./routes/Work.svelte";
 
+	let profileImg = 'img/gillmour.jpg';
+
 	let routes = {
 		"/about": About,
 		"/skills": Skills,
@@ -13,16 +15,38 @@
 	}
 
 </script>
-
-<div class="flex justify-between pt-8 w-5/6 m-auto">
-	<div class="logo text-4xl uppercase font-bold">Gillmour.</div>
-	<nav class="p-4 text-black text-xl">
-		<a href="#/about">About Me <span>//</span></a>
-		<a href="#/skills">What I Do <span>//</span></a>
-		<a href="#/work">My Work <span>//</span></a>
-		<a href="#/contact">Contact</a>
-	</nav>
-</div>
+<!-- Header -->
+<section>
+	<div class="flex justify-between pt-8 w-5/6 m-auto">
+		<div class="logo text-4xl uppercase font-bold">Gillmour.</div>
+		<div class="mobile__nav lg:hidden">
+			<div class="space-y-2">
+				<span class="block w-5 h-0.5 bg-black"></span>
+				<span class="block w-8 h-0.5 bg-black"></span>
+				<span class="block w-8 h-0.5 bg-black"></span>
+			  </div>
+		</div>
+		<nav class="p-4 text-black text-xl flex main__nav">
+			<a href="#/about">About Me <span>//</span></a>
+			<a href="#/skills">What I Do <span>//</span></a>
+			<a href="#/work">My Work <span>//</span></a>
+			<a href="#/contact">Contact</a>
+		</nav>
+	</div>
+</section>
+<!-- Header -->
+<!-- Hero -->
+<section>
+	<div class="grid grid-cols place-content-center lg:grid-cols-2 lg:place-content-between pt-8 w-full gap-y-4">
+		<div class="hero__title items-center order-last lg:order-first md:flex">
+			<h2 class="text-center text-6xl md:self-center">Hi, I'm a <span class="font-extrabold italic">Web Developer</span></h2>
+		</div>
+		<div class="hero__img rounded-full">
+			<img class="m-auto rounded-full p-4 bg-black" src="{profileImg}" alt="Gillmour Tunhira">
+		</div>
+	</div>
+</section>
+<!-- Hero -->
 
 <main>
 		<Router {routes} />
@@ -43,6 +67,11 @@
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
+		}
+	}
+	@media (max-width: 768px){
+		.main__nav{
+			display: none;
 		}
 	}
 </style>
